53f619f7c1b157433b76463543d4e549209f8028
Raghavendra-Mahendrakar <114050900+Raghavendra-Mahendrakar@users.noreply.github.com>
2023-02-23 13:26:09 +0530

    Eureka-Server

---
+++ EureksServer/EureksServer/src/main/java/com/pws/EureksServer/EureksServerApplication.java
@@ -0,0 +1,15 @@
+package com.pws.EureksServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EureksServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EureksServerApplication.class, args);
+	}
+
+}
---
+++ EureksServer/EureksServer/src/test/java/com/pws/EureksServer/EureksServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pws.EureksServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EureksServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
